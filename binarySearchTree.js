dcf2c6d3c466d16d428a50df7d12b65e517de91a
Andrei <andreiflausino@live.com>
2024-05-27 20:52:45 -0300

    insertion + deletion

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -12,9 +12,56 @@ class Tree {
     this.root = buildTree(sortedArray, 0, sortedArray.length - 1);
   }
 
-  insert(value) {}
+  insert(value, root = this.root) {
+    if (root === null) {
+      root = new Node(value);
+      return root;
+    }
+    if (value < root.data) {
+      root.left = this.insert(value, root.left);
+    } else if (value > root.data) {
+      root.right = this.insert(value, root.right);
+    }
 
-  deleteItem(value) {}
+    return root;
+  }
+
+  deleteItem(value, root = this.root) {
+    if (value === root.data) {
+      if (root.left === null && root.right === null) {
+        root = null;
+        return root;
+      } else if (root.left === null && root.right !== null) {
+        let rootRight = root.right;
+        root.right = null;
+        root = rootRight;
+      } else if (root.left !== null && root.right === null) {
+        let rootLeft = root.left;
+        root.left = null;
+        root = rootLeft;
+      } else {
+        let rootRight = root.right;
+        let rootLeft = root.left;
+        if (rootRight.left !== null) {
+          let temp = root.right.left;
+          root.right.left = null;
+          root = temp;
+          root.left = rootLeft;
+          root.right = rootRight;
+        } else {
+          root.right = null;
+          root = rootRight;
+          root.left = rootLeft;
+        }
+      }
+    } else if (value < root.data) {
+      root.left = this.deleteItem(value, root.left);
+    } else if (value > root.data) {
+      root.right = this.deleteItem(value, root.right);
+    }
+
+    return root;
+  }
 }
 
 function buildTree(array, start, end) {
@@ -33,7 +80,7 @@ function buildTree(array, start, end) {
 function dedupeAndSort(array) {
   const set = new Set(array);
   const dedupedArray = Array.from(set);
-  return dedupedArray.sort();
+  return dedupedArray.sort((a, b) => a - b);
 }
 
 const prettyPrint = (node, prefix = "", isLeft = true) => {
@@ -49,5 +96,12 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
   }
 };
 
-let tree = new Tree([1, 2, 3, 4, 5, 6, 7]);
+let tree = new Tree([37, 12, 89, 45, 23, 78, 56, 3, 92, 31]);
+prettyPrint(tree.root);
+tree.insert(9);
+tree.insert(8);
+tree.insert(2);
+
+prettyPrint(tree.root);
+tree.deleteItem(3);
 prettyPrint(tree.root);
